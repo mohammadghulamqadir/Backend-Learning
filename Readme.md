5d87ea59c3bb146c737938e3e065253cbf47b5da
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-02-07 19:57:56 +0500

    Cloudinary and multer middleware

--- Readme.md
+++ Readme.md
@@ -2,3 +2,7 @@ Dev dependency:
 it is used in development mode not in production mode.
 
 dependency:
+
+
+# file upload 
+File upload is doing in the Back-end. File handling using in the third party. File upload majority use by 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploaodCloudinar = async (localFilePath) => {
+    try {
+        if (!localFilePath) return "File is not found"
+        //upload the file on the cloudinary 
+        await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })//file has been uploaded Successfully
+        console.log("File has been Uploaded", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved temp file as the upload operation got failed.
+        return null
+    }
+}
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//     { public_id: "olympic_flag" },
+//     function (error, result) { console.log(result); });
+
+export { }
